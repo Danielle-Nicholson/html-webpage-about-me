aaca5bfd1addab9b222cf7bd5ffb58c5dd70166b
Danielle Nicholson <daniellenicholsonart@gmail.com>
2020-06-30 09:11:51 -0600

    add columns page

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
 console.log( "hello, Dee!");
+// prompt(); = Console.ReadLine(); in C#
+let userInput = prompt("Please enter a number to be cubed: ");
+
+// console.log(); = Console.WriteLine(); in C#
+console.log(userInput ** 3);
